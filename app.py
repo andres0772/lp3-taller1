8198d17f4c5548b5260994c8b37276e0ab36497a
andres0772 <andresesteb0772@gmail.com>
2025-10-21 20:52:27 -0500

    se creio el app del archivo app,py

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ def create_app(config_name='default'):
     Returns:
         Flask: Aplicación Flask configurada
     """
-    # TODO: Crear el objeto 'app'
+    #Crear el objeto 'app'
     app = Flask(__name__)
     
     # Cargar configuración
