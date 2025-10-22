a6b233991aa6deb72f1a73e704d3d42b9203536a
andres0772 <andresesteb0772@gmail.com>
2025-10-21 19:11:00 -0500

    agregacion de app en el archivo app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def create_app(config_name='default'):
         Flask: Aplicación Flask configurada
     """
     # TODO: Crear el objeto 'app'
-
+    app = Flask(__name__)
     
     # Cargar configuración
     app.config.from_object(config[config_name])
